625ecc5eaaf03bc1ee97b03868aa931b1c615348
georges9818 <gngageorges@gmail.com>
2025-10-22 12:13:55 +0000

    0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - fonction void
+* printf - print "Size char int long int long long int float"
+* Return: (0)
+*/
+int main(void)
+{
+int n;
+printf("n of an int: %d\n" , n);
+if (n > 0)
+printf("n > 0 of an if: %d\n", n);
+else if (n == 0)
+printf("n == 0) of a else if: %d\n", n);
+else
+printf("is negative\n");
+return (0);
+}
+ 
