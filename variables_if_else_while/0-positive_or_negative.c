f84c14345b80fdcf30a5ab9f3d4d914e067b9854
georges9818 <gngageorges@gmail.com>
2025-10-22 13:19:45 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
 * main - fonction void
-* printf - print "Ce programme attribuera un nombre aléatoire à la variable n "
+* printf - print "Ce programme aléatoire à la variable n "
 * Return: (0)
 */
 int main(void)
 {
 int n;
-printf("n of an int: %d\n" , n);
+printf("n of an int: %d\n" n = 5);
 if (n > 0)
 printf("is positive\n");
 else if (n == 0)
