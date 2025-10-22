e8f4cfabb792383ce0fd7a51434765f8357bd107
georges9818 <gngageorges@gmail.com>
2025-10-22 09:13:17 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 * main - fonction void
-* printf - print "Size char int long int long long int float double"
+* printf - print "Size char int long int long long int float"
 * Return: (0)
 */
 int main(void)
@@ -11,7 +11,6 @@ printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
-printf("Size of a double: %zu byte(s)\n", sizeof(double));
 return (0);
 }
 
