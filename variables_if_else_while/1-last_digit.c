4d9d574f7541764c75aa0d9fce8ff4dee47415e6
georges9818 <gngageorges@gmail.com>
2025-10-23 11:35:59 +0000

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,16 +17,14 @@ last_digit = n % 10;
 if (last_digit < 0)
 {
 last_digit = -last_digit;
-}
 
-printf("Last digit of %d is %d ", n, last_digit);
 if (last_digit > 5)
 {
-printf("and is greater than 5\n");
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 else if (last_digit == 0)
 {
-printf("and is 0\n");
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 else
 {
