7cf6fba17cbb1169e6a149563713eccb26704a52
georges9818 <gngageorges@gmail.com>
2025-10-23 19:55:16 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - print all single digit numbers of base 10
+*
+* Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = '10'; c <= '0'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
