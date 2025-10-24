ecf80a61c7cab83149969d952f7d679aa83d9784
georges9818 <gngageorges@gmail.com>
2025-10-24 12:08:44 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - check the code
+* 
+* Return: Always
+*\
+int main(void)
+{
+char _putchar;
+for (_putchar = 'a'; _putchar <= 'z'; c++)
+write(1, "'a'<='z'_putchar)12";
+write(1, "_putchar \n"
+return (0);
