ffd1f1e2ccbf49d533ebcee56b8cf71c44081d8f
georges9818 <gngageorges@gmail.com>
2025-10-26 11:05:36 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - qui additionne deux entiers et retourne le résultat
+* @a: premier entier
+* @b: deuxième entier
+*
+* Return: la somme de a et b
+*/
+int add(int a, int b)
+{
+int somme;
+somme = a + b;
+return (somme);
+}
