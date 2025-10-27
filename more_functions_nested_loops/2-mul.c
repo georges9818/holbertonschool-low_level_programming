8d09a7f217fbb3e8040b85fb105d1ea01e84a47f
georges9818 <gngageorges@gmail.com>
2025-10-27 14:58:53 +0000

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul - multiplie deux entiers et retourne le résultat
+* @a: premier entier
+* @b: deuxième entier
+* Return: la somme de a et b
+*/
+int mul(int a, int b)
+{
+int somme;
+somme = a * b;
+return (somme);
+}
