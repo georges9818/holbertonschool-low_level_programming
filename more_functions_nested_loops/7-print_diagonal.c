47527b159c1a9cc4bb5a61f3d98ea5a09211e866
georges9818 <gngageorges9818@gmail.com>
2025-10-28 14:29:48 +0000

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_diagonal - dessine une ligne diagonale sur le terminal
+* @n: nombre de fois que le caractère _ doit être imprimé
+*/
+void print_diagonal(int n)
+{
+int i, j;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
+
