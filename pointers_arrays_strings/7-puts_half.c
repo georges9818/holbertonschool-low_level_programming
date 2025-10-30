ca7db0004ef73f0446d9e5291b5f9ef938cfe78d
georges9818 <gngageorges@gmail.com>
2025-10-30 20:20:28 +0000

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* puts_half - fonction qui affiche la moitié d'une chaîne * @str: pointeur de caractère
+*/
+void puts_half(char *str)
+{
+int longueur = 0;
+int debut;
+while (str[longueur] != '\0')
+{
+longueur++;
+}
+debut = (longueur + 1) / 2;
+while (str[debut] != '\0')
+{
+_putchar(str[debut]);
+debut++;
+}
+_putchar('\n');
+}
+
