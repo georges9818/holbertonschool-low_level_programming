c204524c237e811e332e0dd1657764915525ea50
georges9818 <gngageorges@gmail.com>
2025-10-30 18:28:56 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* puts2 - fonction qui affiche un caractère sur deux d'une chaîne
+* @str: pointeur de caractère
+*/
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
+
