6213257b4afcea45615fbbc71c2070b62262d9ab
georges9818 <gngageorges@gmail.com>
2025-10-31 13:07:10 +0000

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,4 +10,5 @@ char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+void reset_to_98(int *n);
 #endif /* MAIN_H */
